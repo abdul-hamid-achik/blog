2eab5676be2645bfb6a05f719e3bd43471f28d2d
abdul hamid <abdulachik@icloud.com>
2023-04-28 01:10:25 -0600

    disabling hire me tab to avoid possible spam

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
-                  <Link href="/hire-me">Hire me</Link>
+                  {/* TODO: enable hire me link once i figure out how to prevent spam */}
+                  {/*<Link href="/hire-me">Hire me</Link>*/}
                   <Link href="/about">About</Link>
                 </nav>
               </div>
