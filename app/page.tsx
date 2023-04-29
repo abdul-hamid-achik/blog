ebe396ade0e571a40aa0f093153aa0f3ca4df05e
abdul hamid <abdulachik@icloud.com>
2023-04-29 15:34:04 -0600

    sorting posts by date using luxon

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
-import { allPosts } from "@/.contentlayer/generated"
+import {allPosts} from "@/.contentlayer/generated"
 import Link from "next/link"
+import {DateTime} from "luxon"
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
+      <div className="prose dark:prose-invert">
+        {allPosts.sort((first, second) => {
+          const firstDate = DateTime.fromISO(first.date)
+          const secondDate = DateTime.fromISO(second.date)
+
+          return secondDate.toMillis() - firstDate.toMillis()
+        }).map((post) => (
+            <article key={post._id}>
+              <Link href={post.slug}>
+                <h2>{post.title}</h2>
+              </Link>
+              {post.description && <p>{post.description}</p>}
+            </article>
+        ))}
+      </div>
   )
 }
