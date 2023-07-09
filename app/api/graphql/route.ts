0e343eea7ac80a42fc4155b2c20a6e4aa99a53b4
abdul hamid <abdulachik@icloud.com>
2023-07-09 16:55:48 -0600

    enabling cors from options endpoint

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server"
 import { startServerAndCreateNextHandler } from "@as-integrations/next"
+import status from 'http-status'
 import { NextRequest } from "next/server"
 import resolvers from "./resolvers"
 import typeDefs from "./typeDefs"
@@ -20,10 +21,32 @@ const handler = startServerAndCreateNextHandler<NextRequest>(server, options)
 export const dynamic = "force-dynamic"
 export const revalidate = 0
 
+export async function OPTIONS(_request: Request) {
+  const response = new Response(undefined, { status: status.OK });
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  response.headers.set(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+  );
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
+  return response;
+}
+
+
 export async function GET(request: NextRequest) {
-  return handler(request)
+  const response = await handler(request)
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  return response
 }
 
 export async function POST(request: NextRequest) {
-  return handler(request)
+  const response = await handler(request)
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+  return response
 }
--- next.config.js
+++ next.config.js
@@ -13,20 +13,6 @@ const nextConfig = {
       },
     ],
   },
-
-  async headers() {
-    return [
-      {
-        source: "/api/graphql",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = withNextIntl(withContentlayer(nextConfig))
