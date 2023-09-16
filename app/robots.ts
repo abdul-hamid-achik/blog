2ad49e508f3a73f910cedb6538dd795b8ab197c2
abdul hamid <abdulachik@icloud.com>
2023-09-16 15:49:24 -0600

    adding robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { getBaseURL } from '@/lib/utils'
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: `${getBaseURL()}/sitemap.xml`,
+  }
+}
