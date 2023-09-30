3e9877a52e3ec1d37be838eb57350b21f6013582
abdul hamid <abdulachik@icloud.com>
2023-09-30 15:35:33 -0600

    improving charts

--- components/charts.tsx
+++ components/charts.tsx
@@ -64,7 +64,7 @@ export function PostsOverTime() {
   const hsl = theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
 
   return (
-    <div className="h-[200px]">
+    <div className="h-[200px] my-10">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           data={data?.postsOverTime || []}
@@ -95,7 +95,6 @@ export function PostsOverTime() {
     </div>
   )
 }
-
 export function ReadingTimeDistribution() {
   const { data, error } = useQuery(READING_TIME_DISTRIBUTION_QUERY)
   const { theme: mode } = useTheme()
@@ -103,29 +102,70 @@ export function ReadingTimeDistribution() {
   if (error) return <>{error.message}</>
 
   const hsl = theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
-  return (<div className="h-[200px]">
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart
-        width={500}
-        height={300}
-        data={data?.readingTimeDistribution || []}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="category" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar
-          dataKey="count"
-          style={{
-            fill: "var(--theme-primary)",
-            opacity: 1,
-            "--theme-primary": `hsl(${hsl})`,
-          } as React.CSSProperties}
-        />
-      </BarChart>
-    </ResponsiveContainer>
-  </div>
+
+  return (
+    <div className="h-[200px] my-10">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data?.readingTimeDistribution || []}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="category" />
+          <YAxis />
+          <Tooltip
+            content={({ active, payload, label }) => {
+              if (active && payload && payload.length) {
+                const totalPosts = data?.readingTimeDistribution?.reduce((total: number, item: { count: number }) => total + item.count, 0) || 0;
+                const percentage = (((payload[0]?.value || 0) as number) / totalPosts * 100).toFixed(2);
+                return (
+                  <div className="rounded-lg border bg-background p-2 shadow-sm">
+                    <div className="grid grid-cols-2 gap-2">
+                      <div className="flex flex-col">
+                        <span className="text-[0.70rem] uppercase text-muted-foreground">
+                          Category
+                        </span>
+                        <span className="font-bold text-muted-foreground">
+                          {payload[0].payload.category}
+                        </span>
+                      </div>
+                      <div className="flex flex-col">
+                        <span className="text-[0.70rem] uppercase text-muted-foreground">
+                          Count
+                        </span>
+                        <span className="font-bold">
+                          {payload[0].value} ({percentage}%)
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+
+              return null
+            }}
+          />
+
+          <Legend
+            wrapperStyle={{
+              fill: "var(--theme-primary)",
+              opacity: 1,
+              "--theme-primary": `hsl(${hsl})`,
+            } as React.CSSProperties}
+          />
+          <Bar
+            dataKey="count"
+            fill="var(--theme-primary)"
+            style={{
+              fill: "var(--theme-primary)",
+              opacity: 1,
+              "--theme-primary": `hsl(${hsl})`,
+            } as React.CSSProperties}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
